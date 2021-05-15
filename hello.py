5aa2cf07de0ec5dbc7bd5d7e1afcb2586cad3543
Agarg98 <aman1998garg@gmail.com>
2021-05-15 20:37:45 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+
+for i in (range(10)):
+    print("* \n")
+
+
+print(range(10))
+
---
+++ strings.py
@@ -0,0 +1,6 @@
+print("Hello \nWorld", end=". ") # \n creates a new line
+print(*("This is good", "great"), sep=" and ")
+
+# /n creates a new line.
+# end= : This argument tells print statement what character to keep at the end of a line. This is "\n" by default.
+# sep= : This argument specifies the character to separate two
